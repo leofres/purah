5f276c1e6e87a4d4bebaf845f9a41d83e85ef366
monospacedmagic <luci@monospacedmagic.dev>
2020-05-06 12:53:23 +0200

    Add some commands and controller methods

--- extensions/ssbu/cogs/tournaments.py
+++ extensions/ssbu/cogs/tournaments.py
@@ -4,11 +4,40 @@ import hero
 from hero import checks, models
 
 from ..controller import SsbuController
+from ..models import SsbuSettings
 from ..stages import Stage
 
 
 class Tournaments(hero.Cog):
     ctl: SsbuController
+    settings: SsbuSettings
+
+    @hero.Cog.listener()
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        emoji: discord.PartialEmoji = payload.emoji
+        message_id = payload.message_id
+        user_id = payload.user_id
+        channel_id = payload.channel_id
+        guild_id = payload.guild_id
+
+        # Ignore own reactions
+        if user_id == self.core.user.id:
+            return
+
+        # If the reaction comes from a PrivateChannel, it is not relevant here
+        if guild_id is None:
+            return
+
+        # TODO
+
+    @hero.command(hidden=True)
+    @checks.is_owner()
+    async def set_challongecredentials(self, ctx: hero.Context, username: str, apikey: str):
+        self.settings.challonge_username = username
+        self.settings.challonge_api_key = apikey
+        await self.settings.async_save()
+        await self.ctl.initialize_challonge_user()
+        await ctx.send("Challonge credentials set.")
 
     @hero.command()
     async def strike(self, ctx, stage: Stage):
@@ -17,3 +46,18 @@ class Tournaments(hero.Cog):
         # add striked stage to Match's striked stages and
         # edit the striking message accordingly
         pass
+
+
+"""
+teamup
+setteamname
+forfeit
+pick  # stage
+accept  # gentleman agreement on a stage
+reject  # opposite of accept
+won
+lost
+confirm  # confirm that opponent won/lost
+to_setwinner  # moderative command used to set the winner of a match
+# more
+"""
--- extensions/ssbu/controller.py
+++ extensions/ssbu/controller.py
@@ -1,9 +1,26 @@
 import hero
 from hero import async_using_db
 
+import challonge
+
+from .models import SsbuSettings
+
 
 class SsbuController(hero.Controller):
-    # TODO
+    settings: SsbuSettings
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.challonge_user = None
+
+    async def initialize_challonge_user(self):
+        challonge_username = self.settings.challonge_username
+        challonge_api_key = await self.settings.challonge_api_key
+        if challonge_username is None or challonge_api_key is None:
+            return
+        self.challonge_user = await challonge.get_user(challonge_username, challonge_api_key)
+        return self.challonge_user
+
     @async_using_db
     def get_stage(self, ctx, number):
         """Gets the stage with that number given the context
@@ -24,3 +41,45 @@ class SsbuController(hero.Controller):
         # TODO figure out if channel is tournament channel,
         # match channel or neither, then get stagelist
         # from tournament or guild
+
+    async def strike_stage(self, channel, stage):
+        match = self.get_match(channel)
+        await self._strike_stage(match, stage)
+        await self._update_striking_message(channel, match)
+
+    @async_using_db
+    def get_match(self, channel):
+        # TODO get Match/DoublesMatch from channel
+        pass
+
+    @async_using_db
+    def _strike_stage(self, match, stage):
+        # TODO strike stage from game in database
+        pass
+
+    async def _update_striking_message(self, channel, match=None):
+        match = match or self.get_match(channel)
+        message = await channel.fetch_message(match.striking_message.id)
+        # TODO generate new striking message with updated striked stages,
+        # then find out if stage is the newest message with `message.channel.history`;
+        # if so, edit the message; if not, delete it, send a new message and
+        # set the striking_message to the new message (after `db.load`ing it and
+        # async_saving it), then async_saving the match
+
+
+# TODO
+"""
+async def save_tournament
+async def get_challonge_tournament
+cached_tournaments  # use self.cache for this
+async def get_challonge_participant
+async def get_challonge_match
+async def send_signup_message
+async def signup(self, tournament, member) -> Participant
+async def signup_team_member(self, tournament, member)
+async def send_checkin_message
+async def checkin(self, tournament, member) -> Participant
+async def checkin_team_member(self, tournament, member)
+async def send_match_intro
+# more
+"""
